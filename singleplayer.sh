41b3113c3f63d44cdff424f56c704aa51985bc40
Randall E. Barker <simstorm@mac.com>
2008-11-13 17:43:09 -0800

    updating cycles

--- singleplayer.sh
+++ singleplayer.sh
@@ -2,4 +2,4 @@
 
 . ../scripts/envsetup.sh
 
-$BIN_HOME/dmzAppQt -f config/resource.xml config/runtime.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/cycle.xml config/game.xml config/lua.xml config/cycle_yellow.xml config/mcp.xml config/drone.xml
+$BIN_HOME/dmzAppQt -f config/resource.xml config/runtime.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/cycle.xml config/game.xml config/lua.xml config/cycle_yellow.xml config/mcp.xml config/drone.xml config/render.xml
