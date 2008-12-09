452031e98dfdc313a9aa53da6ad11d2137371d63
Randall E. Barker <simstorm@mac.com>
2008-12-08 17:25:33 -0800

    Updated to support DMZ_DEBUG env variable.

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 . ../scripts/envsetup.sh
 
-$BIN_HOME/dmzAppQt -f config/resource.xml config/runtime.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/lua.xml $*
+$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/resource.xml config/runtime.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/lua.xml $*
