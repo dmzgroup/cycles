829df186ebd613f1af1bffcc86981055e7878c79
Randall E. Barker <simstorm@mac.com>
2010-05-21 16:59:00 -0700

    Updated networking configs so transmition rates are lower.

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ if [ "$1" = "" ] ; then
    export EXTRA_FILE=config/cycle_red.xml ;
 fi
 
-$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/resource.xml config/runtime.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/cycle_overlay.xml config/cycle.xml config/game.xml config/lua.xml $* $EXTRA_FILE
+$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/render.xml config/resource.xml config/runtime.xml config/common.xml config/audio.xml config/input.xml config/cycle_overlay.xml config/cycle.xml config/game.xml config/lua.xml config/net.xml $* $EXTRA_FILE
