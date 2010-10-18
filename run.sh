865911bb157b17759e4a1c93aaf6cdfcb42113d1
Randall E. Barker <simstorm@mac.com>
2010-10-18 16:13:59 -0700

    Added app names

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ if [ "$1" = "" ] ; then
    export EXTRA_FILE=config/cycle_red.xml ;
 fi
 
+export DMZ_APP_NAME=cycles
+
 $RUN_DEBUG$BIN_HOME/dmzAppQt -f config/audio.xml config/render.xml config/resource.xml config/runtime.xml config/common.xml config/js.xml config/input.xml config/cycle_overlay.xml config/cycle.xml config/game.xml  config/net.xml $* $EXTRA_FILE
--- singleplayer.sh
+++ singleplayer.sh
@@ -2,4 +2,6 @@
 
 . ../scripts/envsetup.sh
 
+export DMZ_APP_NAME=cycles
+
 $RUN_DEBUG$BIN_HOME/dmzAppQt -f config/audio.xml config/render.xml config/resource.xml config/runtime.xml config/common.xml config/input.xml config/cycle.xml config/game.xml  config/js.xml config/cycle_yellow.xml config/mcp.xml config/drone.xml config/cycle_overlay.xml config/net.xml $*
